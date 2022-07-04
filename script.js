5621c4709d2d25f87b6a4d0a0bd9173f2fc00867
PaulaMerlim <paula.merlim.77@gmail.com>
2022-07-04 19:14:13 -0300

    Desafio da aula 22-06

---
+++ script.js
@@ -0,0 +1,31 @@
+// Crie um array que receba 5 itens e exiba no console.
+let amigos = ["Paula","Debora","João","Leno","Jordan"];
+console.log (amigos [0])
+console.log (amigos [1])
+console.log (amigos[2])
+console.log (amigos[3])
+console.log (amigos[4])
+
+// Utilize um método para adicionar um nome ao inicio do array.
+let amig = ["Paula","Debora","João","Leno","Jordan"];
+amig.unshift ("Aline")
+console.log (amig)
+
+// Utilize um método para remover o último nome do array.
+let friends = ["Paula","Debora","João","Leno","Jordan"];
+friends.pop ();
+console.log (friends);
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+let alunos = ["Paula","Debora","João","Leno","Jordan"];
+alunos.push ("Daniel","Robert");
+console.log (alunos);
+// Utilize um método para remover o primeiro nome do array.
+let brothers = ["Paula","Debora","João","Leno","Jordan"];
+brothers.shift ();
+console.log (brothers);
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+
+// let numbers = [7,5,6,3,8,9,2,1,4]
+
